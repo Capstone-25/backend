56019eaf0e76a716b3067ce137749a31000ebf00
Ss0Mae <ssomae@protonmail.ch>
2025-04-10 21:13:10 +0900

    [Feat] Swagger 서버 연결

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,35 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
+import { AllExceptionsFilter } from './common/exceptions/all-exceptions.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Capstone API')
+    .setDescription('Capstone을 위한 REST API 문서입니다.')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 인증 추가
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document, {
+    swaggerOptions: {
+      url: '/api-docs', // Swagger 문서 경로와 맞추기 위해 설정
+    },
+    customSiteTitle: 'API 문서',
+  });
+
+  app.useGlobalFilters(new AllExceptionsFilter());
   await app.listen(process.env.PORT ?? 8080);
 }
 bootstrap();
