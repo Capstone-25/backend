c76e7ea7beb5e7fb07652624e6639593bf52f58f
Ss0Mae <ssomae@protonmail.ch>
2025-04-11 20:57:09 +0900

    [Feat] Auth 관련 컨트롤러 모듈 구현

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import {
 import { AuthGuard } from '@nestjs/passport';
 import { Response } from 'express';
 import { JwtAuthGuard } from '@src/modules/auth/guards/jwt-auth.guard';
+import { AuthService } from './auth.service';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -27,10 +28,10 @@ export class AuthController {
     return {
       message: '구글 소셜 로그인 성공',
       data: {
-        userId: user.id,
+        userId: user.userId,
         email: user.email,
         name: user.name,
-        authProvider: user.authProvide,
+        authProvider: user.authProvider,
         accessToken: accessToken,
         isExistingUser,
       },
@@ -41,22 +42,33 @@ export class AuthController {
   @UseGuards(JwtAuthGuard)
   async logout(@Req() req: any, @Res() res: Response) {
     const userId = req.user?.userId;
-    await this.authService.deleteRefreshToken(userId);
     return {
       message: '로그아웃 성공',
     };
   }
-    
-    @Post('signup')
-    async signup(@Body() body: { email: string, name: string, password: string }) {
-        const result = await this.authService.signup(
-            body.email,
-            body.name,
-            body.password
-        );
-        return {
-            message: '일반 회원가입 성공',
-            data:result
-        }
-    }
+
+  @Post('signup')
+  async signup(
+    @Body() body: { email: string; name: string; password: string }
+  ) {
+    const result = await this.authService.signup(
+      body.email,
+      body.name,
+      body.password
+    );
+    return {
+      message: '일반 회원가입 성공',
+      data: result,
+    };
+  }
+
+  @Post('login')
+  async login(@Body() body: { email: string; password: string }) {
+    const result = await this.authService.login(body.email, body.password);
+    return {
+      message: '일반 로그인 성공',
+      user: result.user,
+      accessToken: result.accessToken,
+    };
+  }
 }
