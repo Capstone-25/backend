3aa7f40cdf5df4a195dbc84c1c335d281adfe792
Ss0Mae <ssomae@protonmail.ch>
2025-04-11 20:57:24 +0900

    [Feat] 메인 모듈 수정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 환경 변수를 글로벌하게 사용할 수 있도록 설정
+    }),
+    AuthModule
+  ],
 })
 export class AppModule {}
