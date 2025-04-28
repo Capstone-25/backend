09cc998a3fa42aced534dc7f77dd57e80e613a16
Ss0Mae <ssomae@protonmail.ch>
2025-04-28 16:41:53 +0900

    [Feat] Google Strategy 고도화

--- src/modules/auth/strategies/google.strategy.ts
+++ src/modules/auth/strategies/google.strategy.ts
@@ -10,7 +10,9 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL: process.env.GOOGLE_CALLBACK_DEPLOY_URL,
-      scope: ['email', 'profile'],
+      scope: ['email', 'profile', 'https://www.googleapis.com/auth/calendar'],
+      accessType: 'offline', // refresh token 발급 받기 위해필수
+      prompt: 'consent',
     });
   }
 
@@ -19,18 +21,20 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     refreshToken: string,
     profile: any,
     done: VerifyCallback
-  ): Promise<AuthUserDto> {
-    const { id, emails, displayName, photos } = profile;
-
-    if (!emails || emails.length === 0) {
-      throw new Error('No email associated with this Google account');
-    }
-
-    return {
+  ) {
+    const { emails, displayName, photos } = profile;
+    const userDto: AuthUserDto = {
       email: emails[0].value,
       name: displayName,
-      profileImageUrl: photos[0]?.value || null,
+      profileImageUrl: photos[0]?.value,
       authProvider: 'google',
     };
+    // PassportContext에 토큰까지 붙여서 넘겨줍니다.
+    done(null, {
+      ...userDto,
+      googleAccessToken: accessToken,
+      googleRefreshToken: refreshToken,
+      // (옵션) 만료시간 계산 후 전달
+    });
   }
 }
