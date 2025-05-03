18c03c74c622658cbc5f857826525f5b8d739df6
Ss0Mae <ssomae@protonmail.ch>
2025-05-03 22:55:13 +0900

    [Feat] UserModule 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { CalendarModule } from './modules/calendar/calendar.module';
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true, // 환경 변수를 글로벌하게 사용할 수 있도록 설정
     }),
-    AuthModule
+    AuthModule,
+    CalendarModule,
+    ChatModule,
   ],
 })
 export class AppModule {}
