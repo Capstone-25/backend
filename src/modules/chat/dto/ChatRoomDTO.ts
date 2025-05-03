e54193e41952f77cb6d112953a88ccaeef33a02f
Ss0Mae <ssomae@protonmail.ch>
2025-05-03 22:56:55 +0900

    [Docs] chat DTO 관련 Swagger 주석

--- src/modules/chat/dto/ChatRoomDTO.ts
+++ src/modules/chat/dto/ChatRoomDTO.ts
@@ -1,17 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-export class ChatMessageDTO {
-  @ApiProperty({
-    description: '메시지 ID',
-    example: 1,
-  })
-  id: number;
-
+export class ChatRoomDTO {
   @ApiProperty({
     description: '채팅방 ID',
     example: 1,
   })
-  chatId: number;
+  id: number;
 
   @ApiProperty({
     description: '사용자 ID',
@@ -20,13 +14,13 @@ export class ChatMessageDTO {
   userId: number;
 
   @ApiProperty({
-    description: '메시지 내용',
-    example: '안녕하세요',
+    description: '채팅방 제목',
+    example: '첫 번째 대화',
   })
-  message: string;
+  title: string;
 
   @ApiProperty({
-    description: '메시지 생성 시간',
+    description: '채팅방 생성 시간',
     example: '2024-05-03T12:00:00.000Z',
   })
   createdAt: Date;
--- src/modules/chat/dto/CreateSessionDTO.ts
+++ src/modules/chat/dto/CreateSessionDTO.ts
@@ -1,3 +1,10 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateSessionDto {
+  @ApiProperty({
+    description: '채팅방 제목',
+    required: false,
+    example: '첫 번째 대화',
+  })
   title?: string;
 }
