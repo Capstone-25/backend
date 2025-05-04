027de2984b7aff919d64fcd1cbcd2ff8fed03330
Ss0Mae <ssomae@protonmail.ch>
2025-05-04 23:58:51 +0900

    [Feat] Firebase 서비스 계정 JSON 파일 추가 및 Firebase 초기화 방식 수정

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,26 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import * as admin from 'firebase-admin';
+import * as serviceAccount from './firebase-service-account.json';
 
 @Injectable()
 export class FirebaseConfig {
   constructor() {
-    if (!process.env.FIREBASE_PRIVATE_KEY) {
-      throw new Error('FIREBASE_PRIVATE_KEY is not defined');
-    }
-
-    // private key에서 실제 줄바꿈 문자로 변환
-    const privateKey = process.env.FIREBASE_PRIVATE_KEY.replace(
-      /\\n/g,
-      '\n'
-    ).replace(/"/g, ''); // 따옴표 제거
     try {
       admin.initializeApp({
-        credential: admin.credential.cert({
-          projectId: process.env.FIREBASE_PROJECT_ID,
-          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-          privateKey: privateKey,
-        }),
+        credential: admin.credential.cert(
+          serviceAccount as admin.ServiceAccount
+        ),
       });
+      console.log('Firebase initialized successfully');
     } catch (error) {
       console.error('Firebase 초기화 실패:', error);
       throw error;
