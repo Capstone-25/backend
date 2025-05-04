01f63367727d1588e5b175aec835c01d98dd48a9
Ss0Mae <ssomae@protonmail.ch>
2025-05-04 22:47:27 +0900

    [Feat] Firebase config 설정

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBj-ZS1T9Tm46EgOaCPiixDYS8W2RFwYBU",
+  authDomain: "capstone-415ab.firebaseapp.com",
+  projectId: "capstone-415ab",
+  storageBucket: "capstone-415ab.firebasestorage.app",
+  messagingSenderId: "729746385693",
+  appId: "1:729746385693:web:7b643178674d1b4895cff4",
+  measurementId: "G-5W92P03JQ5"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/firebase-logo.png'
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+}); 
